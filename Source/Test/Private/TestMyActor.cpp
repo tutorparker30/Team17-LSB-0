afc7e9039f49beb747f2c226b5e02a9fa8bd18f1
leesubindlqslek <rftnqls@naver.com>
2025-02-10 09:33:25 +0900

    새로운 액터 클래스 추가
    팀원이 커밋하고 푸시한 상황을 훈련해보기 위해서 새로운 액터 클래스 생성

---
+++ Source/Test/Private/TestMyActor.cpp
@@ -0,0 +1,23 @@
+
+
+
+#include "TestMyActor.h"
+
+ATestMyActor::ATestMyActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ATestMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ATestMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
